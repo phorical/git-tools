62bd147a4112b7ff9399f750e8004eb1b5e7e503
George Pirogov <digitalduke@gmail.com>
2018-10-05 22:28:44 +0500

    A simple test of how to get the list of today closed issues.

---
+++ mockup/test-pygithub/get_issues.py
@@ -0,0 +1,15 @@
+from datetime import date, datetime
+from github import Github
+
+ACCESS_TOKEN = ""
+
+today = datetime.combine(date.today(), datetime.min.time())
+
+github = Github(ACCESS_TOKEN)
+user = github.get_user()
+
+closed_issues = user.get_issues(state="closed", since=today)
+print('daily closed issues:', closed_issues.totalCount)
+
+for issue in closed_issues:
+    print(issue.number, issue.title)
