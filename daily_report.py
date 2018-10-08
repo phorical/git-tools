ebbbd814d0b3541bc10eba98e4f2b39eb52abe5c
George Pirogov <digitalduke@gmail.com>
2018-10-08 23:25:00 +0500

    Make lists without markdown syntax.

--- daily_report.py
+++ daily_report.py
@@ -18,7 +18,7 @@ try:
     closed_issues = user.get_issues(state="closed", since=today)
     print('Daily closed issues:', closed_issues.totalCount)
     for issue in closed_issues:
-        print('[%s](%s) %s' % (issue.number, issue.html_url, issue.title))
+        print(issue.number, issue.html_url, issue.title)
     print()
 except (Exception, BadCredentialsException) as error:
     print("Can't get list of issues.")
@@ -28,7 +28,7 @@ try:
     print('Daily commits in repo "%s" total: %s' % (REPO_NAME, commits.totalCount))
     for commit in commits:
         if not commit.commit.message.startswith("Merge"):
-            print('[%s](%s) %s' % (commit.sha[:7], commit.html_url, commit.commit.message))
+            print(commit.sha[:7], commit.html_url, commit.commit.message)
 except (Exception, BadCredentialsException) as error:
     print("Can't get list of commits.")
 
