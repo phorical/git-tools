cea985a1448c6fcc0bef0668980797380c43c681
George Pirogov <digitalduke@gmail.com>
2018-11-21 20:43:12 +0500

    Deleting unnecessary files from gh-pages branch.

--- mockup/test-pygithub/test_pygithub.py
+++
@@ -1,7 +0,0 @@
-from github import Github
-
-ACCESS_TOKEN = ""
-g = Github(ACCESS_TOKEN)
-
-for repo in g.get_user().get_repos():
-    print(repo.name)
--- purge-branches.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Удалить все ветки, кроме master и которые, не смержены в текущую ветку.
-
-git branch --merged | grep -v "\*" | grep -v master | xargs -n 1 git branch -d
-
